d3606c1527f232649eac1bb98b2cb748776b4efa
Charles Magalhaes <charlesrm29@gmail.com>
2022-10-03 23:59:02 -0300

    Implementação do método exibirNumNaturais

---
+++ src/QuestaoR2/NumerosNaturais.java
@@ -0,0 +1,24 @@
+package QuestaoR2;
+
+public class NumerosNaturais {
+	private int n;
+
+	public NumerosNaturais(int n) {
+		this.n = n;
+	}
+
+	public NumerosNaturais() {
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+	
+	public String exibirNumNaturais() {
+		
+	}
+	
+	
+	
+	
+}
---
+++ src/QuestaoR2/aplicacao.java
@@ -0,0 +1,10 @@
+package QuestaoR2;
+
+public class aplicacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
